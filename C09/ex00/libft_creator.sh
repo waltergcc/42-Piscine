113d216abd728c5e2503b80e9041a2ebaeb1d9fc
wcorrea- <waltergccruz@gmail.com>
2023-03-02 21:57:03 +0000

    C09

--- C09/ex00/libft_creator.sh
+++ C09/ex00/libft_creator.sh
@@ -11,5 +11,9 @@
 # **************************************************************************** #
 
 #!/bin/bash
+
+# This script compiles the source files in object files
+# and groups them in a libft.a static library
+# This library can be used in other projects in C
 gcc -Wall -Wextra -Werror -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c
 ar rc libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o

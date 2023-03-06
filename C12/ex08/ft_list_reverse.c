34ac71861cf3329d6444ac23692bfff50a10af3d
wcorrea- <waltergccruz@gmail.com>
2023-03-06 17:16:49 +0000

    C12: ex08 done

--- C12/ex08/ft_list_reverse.c
+++ C12/ex08/ft_list_reverse.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_list_last.c                                     :+:      :+:    :+:   */
+/*   ft_list_reverse.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wcorrea- <wcorrea-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 15:39:02 by wcorrea-          #+#    #+#             */
-/*   Updated: 2023/03/06 17:02:41 by wcorrea-         ###   ########.fr       */
+/*   Updated: 2023/03/06 17:16:16 by wcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Show the last value of the list
-t_list	*ft_list_last(t_list *begin_list)
+// reverses the values on the list
+void ft_list_reverse(t_list **begin_list)
 {
-	// If the list is empty returns null
-	if (!begin_list)
-		return (NULL);
-	
-	// Until it reaches the last value of the list
-	// 'begin_list' receives the value of next
-	while (begin_list->next)
-		begin_list = begin_list->next;
+	t_list	*prev;
+	t_list	*current;
+	t_list	*next;
 
-	// The last value stored in 'begin_list' is returned
-	return (begin_list);
+	current = (*begin_list);
+	prev = NULL;
+	next = NULL;
+	while(current != NULL)
+	{
+		// 'Next' keeps the address of the next position
+		next = current->next;
+
+		// The next position receives the previous
+		current->next = prev;
+
+		// The previous position receives the current
+		prev = current;
+
+		// The current position receives the address that was stored in Next
+		current = next;
+	}
+	*begin_list = prev;
 }
 /* 
 // Creates a t_list element that is a struct
@@ -53,7 +64,7 @@ void	print_list(t_list *list)
 {
 	t_list *current = list;
 
-	while (current != NULL)
+	while (current)
 	{
 		printf("Value: %d\n", *(int *)current->data);
 		current = current->next;
@@ -73,11 +84,10 @@ int main(void)
 	linked_list = ft_create_elem(ptr1);
 	linked_list->next = ft_create_elem(ptr2);
 	linked_list->next->next = ft_create_elem(ptr3);
-	printf("All values of list:\n");
+	printf("Linked list before:\n");
 	print_list(linked_list);
-	
-	// Show the last value of the list
-	printf("The last value of list:\n");
-	print_list(ft_list_last(linked_list));
+	ft_list_reverse(&linked_list);
+	printf("Linked list after:\n");
+	print_list(linked_list);
 	free(linked_list);
 } */

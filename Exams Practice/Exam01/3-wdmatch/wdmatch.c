7bc146ec0ce5fd1b2b27c52f1edd25f403794694
wcorrea- <waltergccruz@gmail.com>
2023-03-08 21:14:15 +0000

    Exam02 all commented

--- Exams Practice/Exam01/3-wdmatch/wdmatch.c
+++ Exams Practice/Exam01/3-wdmatch/wdmatch.c
@@ -2,53 +2,63 @@
 
 void	ft_putstr(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+
+	// While the string doesn't reach the null
+	while (str[i])
 	{
+		// Each Char passed by the index "i" will be printed
 		write(1, &str[i], 1);
 		i++;
-	}
+	}	
 }
 
-int		ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
+	// While the string doesn't reach the null
+	// the counter "i" will be increased
 	while (str[i] != '\0')
 		i++;
+
+	// In the end the function returns the number of characters counted
 	return (i);
 }
 
 int		main(int ac, char **av)
 {
-	int i;
-	int j;
-	int wdlen;
+	int i = 0;
+	int j = 0;
+	int match = 0;
 
-	i = 0;
-	j = 0;
-	wdlen = 0;
+	// Check if there are exactly 3 arguments
 	if (ac == 3)
 	{
-		while (av[1][i] != '\0')
+		while (av[1][i])
 		{
-			while (av[2][j] != '\0')
+			while (av[2][j])
 			{
+				// if the char of argument 1 is equal to argument 2
 				if (av[1][i] == av[2][j])
 				{
-					wdlen++;
+					// The counter is increased
+					match++;
+
+					// Get out of the current loop
 					break ;
 				}
 				j++;
 			}
 			i++;
 		}
-		if (wdlen == ft_strlen(av[1]))
+		// If the amount of match is equal to the length, print the string
+		if (match == ft_strlen(av[1]))
 			ft_putstr(av[1]);
 	}
+	// Print the new line in the end
 	write(1, "\n", 1);
-	return (0);
 }
--- Exams Practice/Exam01/5-ft_split/ft_split.c
+++ Exams Practice/Exam01/5-ft_split/ft_split.c
@@ -1,95 +1,143 @@
+// #include <stdio.h>
 #include <stdlib.h>
-int	check_separator(char c)
+
+// check if a character is a separator
+int	is_separator(char c, char *sep)
 {
-	if ( c == 10 || c == 9 || c == 32)
-		return (1);
-	if (c == 0)
-		return (1);
+	int	i;
+
+	i = 0;
+	// goes through all the characters of 'sep'
+	while (sep[i])
+	{
+		// If it's any of the separators, the return is 1
+		if (c == sep[i])
+			return (1);
+		i++;
+	}
+
+	// If not a separator the return is 0
 	return (0);
 }
 
-int	count_strings(char *str)
+int	count_words(char *str, char *sep)
 {
 	int	i;
-	int	count;
+	int	words;
 
-	count = 0;
 	i = 0;
-	while (str[i] != '\0')
+	words = 0;
+
+	// runs all the string
+	while (str[i])
 	{
-		while (str[i] != '\0' && check_separator(str[i]))
-			i++;
-		if (str[i] != '\0')
-			count++;
-		while (str[i] != '\0' && !check_separator(str[i]))
+		// If it's not a separate, 'words' is increased
+		// if it's a separator, 'i' is increased (go to next char)
+		if (!is_separator(str[i], sep))
+		{
+			words++;
+			
+			// While the string is not over and is not a separator, 'i' is increased
+			while (str[i] && !is_separator(str[i], sep))
+				i++;
+		}
+		else
 			i++;
 	}
-	return (count);
+
+	// returns the count of words
+	return (words);
 }
 
-int	ft_strlen_sep(char *str)
+char	*word_splitter(char *str, char *sep)
 {
-	int	i;
-
-	i = 0;
-	while (str[i] && !check_separator(str[i]))
-		i++;
-	return (i);
-}
-
-char	*ft_word(char *str)
-{
-	int		len_word;
-	int		i;
 	char	*word;
+	int		i;
 
 	i = 0;
-	len_word = ft_strlen_sep(str);
-	word = (char *)malloc(sizeof(char) * (len_word + 1));
-	while (i < len_word)
+
+	// Count the lenght of the word until it reachs the end and is not a separator
+	while (str[i] && !is_separator(str[i], sep))
+		i++;
+
+	// allocate the memory to 'Word' in the size counted
+	word = (char *) malloc(sizeof(char *) * (i + 1));
+	if (word == NULL)
+		return (NULL);
+
+	// 'i' needs to receive 0
+	i = 0;
+
+	// While the string is not over and is not a separator
+	// 'Word' receives what is in string
+	while (str[i] && !is_separator(str[i], sep))
 	{
 		word[i] = str[i];
 		i++;
 	}
 	word[i] = '\0';
+
+	// Returns the splitted string
 	return (word);
 }
 
-char	**ft_split(char *str)
+char	**ft_split(char *str, char *charset)
 {
-	char	**strings;
 	int		i;
+	int		j;
+
+	// 'Words' is a set of strings
+	char	**words;
 
 	i = 0;
-	strings = (char **)malloc(sizeof(char *)
-			* (count_strings(str) + 1));
-	while (*str != '\0')
-	{
-		while (*str != '\0' && check_separator(*str))
-			str++;
-		if (*str != '\0')
-		{
-			strings[i] = ft_word(str);
-			i++;
-		}
-		while (*str && !check_separator(*str))
-			str++;
-	}
-	strings[i] = 0;
-	return (strings);
-}
+	j = 0;
 
-#include <stdio.h>
-int	main(int argc, char **argv)
-{
-	int		index;
-	char	**split;
-	(void)	argc;
-	split = ft_split(argv[1]);
-	index = 0;
-	while (split[index])
+	// 'Words' receives memory allocation in the size of the words + 1
+	words = (char **) malloc(sizeof(char *) * (count_words(str, charset) + 1));
+
+	// If any of the strings is null, the return is null
+	if (words == NULL || str == NULL || charset == NULL)
+		return (NULL);
+
+	// runs all the string
+	while (str[i])
 	{
-		printf("%s\n", split[index]);
-		index++;
+		// If it's not a separate, 'words' receives the word splitted
+		if (!is_separator(str[i], charset))
+		{
+			words[j] = word_splitter(&str[i], charset);
+
+			// While the string is not over and is not a separator, 'i' is increased
+			while (str[i] && !is_separator(str[i], charset))
+				i++;
+			
+			// Go to the next index of Words
+			j++;
+		}
+
+		// if it's a separator, 'i' is increased (go to next char)
+		else
+			i++;
 	}
+
+	// In the end Words need to receive 0
+	words[j] = 0;
+	return (words);
 }
+/* 
+int	main(void)
+{
+	char	*str = "Words to.be/splitted*in@six";
+	char	*sep = "./*@! ";
+	char	**words = ft_split(str, sep);
+	int i = 0;
+
+	printf("Before: %s\nAfter: \n", str);
+	while (words[i])
+	{
+		printf("%s\n", words[i]);
+		free(words[i]);
+		i++;
+	}
+	free(words);
+} */
